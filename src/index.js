47c2d0d6377ea0ff10e78838349704b9f8725fc6
sonicsml <sonics.scs@gmail.com>
2019-03-22 19:22:01 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 == 0) {
+  	var count = 0;
+  	for (var i = 0; i < str.length; i++) {
+  		if (( str[i] == '(' ) || (str[i] == '[') || (str[i] == '{')) {
+  			count++;
+  		} 
+  		else if ((str[i] == ')') || (str[i] == ']') || (str[i] == '}')) {
+  			count--;
+  			if (count < 0) {
+  				return false;
+  			}
+  		}
+  	}
+	if (count != 0) {
+		return false;
+	}
+	else return true;
+  }
 }
